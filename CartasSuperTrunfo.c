d5551192bf585b0d0da6ea1ee41f7ffa10582ebe
Soffi77 <rane.barbosa.77@gmail.com>
2025-03-05 17:36:21 +0000

    teste 02

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
 // Desafio Super trunfo.c
+int main(){
+     int populacao;
+     int pontosturisticos;
+     float area;
+     float pib;
+     char cidade;
+     char código;
+     char estado;
 
-int main (){
-     int populacao,pontosturisticos;
-     float area,pib;
-     char cidade,código,estado;
+     printf("Populacao: \n");
+     scanf ("%f", populacao);
 
-     printf("População: \n");
-     scanf ("%s", populacao);
-
-     printf("pontosturisticos: \n");
+     printf("pontos turisticos: \n");
      scanf ("%s", pontosturisticos);
 
      printf("area: \n");
      scanf ("%f", &area);
-
-     printf("pib: \n");
+     
+     printf("pib:\n");
      scanf ("%f", &pib);
 
-     printf("cidade: \n");
+     printf("cidade:\n");
      scanf ("%s", cidade);
 
-     printf("codigo: \n");
+     printf("codigo:\n");
      scanf ("%s", código);
 
      printf("estado: \n");
      scanf ("%s", estado); 
+
      return 0;
 }
