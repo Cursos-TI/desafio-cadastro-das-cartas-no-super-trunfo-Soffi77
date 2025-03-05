f679f451c4dc32c24ccf48cf2d6e3d13b4184f21
Soffi77 <rane.barbosa.77@gmail.com>
2025-03-05 16:32:39 +0000

    finalizado,leitura 01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,25 @@ int main (){
      float area,pib;
      char cidade,código,estado;
 
-     printf
+     printf("População: \n");
+     scanf ("%s", populacao);
 
-    
+     printf("pontosturisticos: \n");
+     scanf ("%s", pontosturisticos);
+
+     printf("area: \n");
+     scanf ("%f", &area);
+
+     printf("pib: \n");
+     scanf ("%f", &pib);
+
+     printf("cidade: \n");
+     scanf ("%s", cidade);
+
+     printf("codigo: \n");
+     scanf ("%s", código);
+
+     printf("estado: \n");
+     scanf ("%s", estado); 
+     return 0;
 }
